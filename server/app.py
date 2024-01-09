ed421b44d989e9cc4f248fb32c47e0376a97bff0
bonganimasemola <bonganimasemola@icloud.com>
2024-01-09 04:24:33 +0300

    flask application structure lesson complete

--- server/app.py
+++ server/app.py
@@ -3,3 +3,10 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return f'<h1>Profile for {username}</h1>'
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
